9656391496e65d3e13f230594b60448de456db79
makssssko <greenmaks25@gmail.com>
2024-05-30 16:53:36 +0300

    new

--- app/handlers.py
+++ app/handlers.py
@@ -1,7 +1,8 @@
 from aiogram import Router, F
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.enums import ParseMode
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery, InputMediaPhoto
+
 import app.keyboards as kb
 
 router = Router()
@@ -9,3 +10,33 @@ router = Router()
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     await message.answer('<b>Вжухх !</b>\n\nСкорее добавляй свои книги в "Прочитанные" и я подберу тебе что-нибудь.',reply_markup=kb.main_menu,parse_mode=ParseMode.HTML)
+
+Objects = [{'image': 'https://sun9-44.userapi.com/impf/c631929/v631929682/1d7b9/rFSOkvhanto.jpg?size=604x496&quality=96&sign=037d7a494759a107dd54d1d87886b891&type=album','name':'Как жить с огромным членом ?','author':'Поркшеян В.М','description':'Описание описание описание описание описание описание'},
+           {'image': 'https://aigis.club/uploads/posts/2022-05/1653072677_11-adonius-club-p-oboi-s-obezyanami-krasivie-15.jpg','name':'Почему я преподаю ИИ','author':'Ляхницкая О.В','description':'Описание описание описание описание описание описание'},
+           {'image': 'https://zefirka.net/wp-content/uploads/2020/06/serdityj-kot-kotoryj-oxranyaet-arbuznuyu-fermu-v-tailande-1.jpg','name':'Биография','author':'Иванов И.И','description':'Описание описание описание описание описание описание'}
+           ]
+# Здесь index сделал чисто для теста и обьекты тоже, потом когда богдан сделает свою часть уберем
+index = 1
+@router.message(F.text == '\U0001F4DA Прочитанные')
+async def cmd_books(message: Message):
+    await message.answer_photo(Objects[index]['image'],f'<b>{Objects[index]["author"]} - {Objects[index]["name"]}</b>\n\n{Objects[index]["description"]}',
+                               reply_markup=kb.get_swiperMenu(index,len(Objects)),
+                               parse_mode=ParseMode.HTML)
+
+@router.callback_query(F.data == 'next')
+async def nextBook(callback: CallbackQuery):
+    await callback.message.edit_media(InputMediaPhoto(media=Objects[index+1]['image'],
+                                                      caption=f'<b>{Objects[index+1]["author"]} - {Objects[index+1]["name"]}</b>\n\n{Objects[index+1]["description"]}',
+                                                      parse_mode=ParseMode.HTML,
+                                                      ))
+    await callback.message.edit_reply_markup(reply_markup=kb.get_swiperMenu(index+1,len(Objects)))
+
+
+@router.callback_query(F.data == 'prev')
+async def prevBook(callback: CallbackQuery):
+    await callback.message.edit_media(InputMediaPhoto(media=Objects[index-1]['image'],
+                                                      caption=f'<b>{Objects[index-1]["author"]} - {Objects[index-1]["name"]}</b>\n\n{Objects[index-1]["description"]}',
+                                                      parse_mode=ParseMode.HTML,
+                                                      ))
+    await callback.message.edit_reply_markup(reply_markup=kb.get_swiperMenu(index-1,len(Objects)))
+
--- app/keyboards.py
+++ app/keyboards.py
@@ -5,3 +5,15 @@ main_menu = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="\U0001F4DA Прочитанные"),KeyboardButton(text="\U00002B50 Рекомендации")],
     [KeyboardButton(text="\U0001F495 Избранное")],
 ],resize_keyboard=True)
+
+def get_swiperMenu(index: int,length: int):
+    keyboard = [[]]
+    if index > 0: keyboard[0].append(InlineKeyboardButton(text="<<",callback_data="prev"))
+    keyboard[0].append(InlineKeyboardButton(text=f'({index+1}/{length})', callback_data="a"))
+    if index < length - 1: keyboard[0].append(InlineKeyboardButton(text=">>",callback_data="next"))
+
+    keyboard.append([InlineKeyboardButton(text='\U00002705 Добавить в избранное', callback_data="addFavourite")])
+    return InlineKeyboardMarkup(inline_keyboard=keyboard)
+
+
+
